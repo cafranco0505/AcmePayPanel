a4178e475122412e27fab63d41943e7b23126246
Carlos Franco <cafranco0505@gmail.com>
2021-01-09 23:49:20 -0500

    CRUD asesores api node - localhost

--- build/controllers/indexController.js
+++ build/controllers/indexController.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getEmployees = void 0;
+exports.deleteEmployee = exports.updateEmployee = exports.createEmployee = exports.getEmployeesById = exports.getEmployees = void 0;
 const db_1 = require("../db");
 const getEmployees = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
@@ -23,3 +23,62 @@ const getEmployees = (req, res) => __awaiter(void 0, void 0, void 0, function* (
     }
 });
 exports.getEmployees = getEmployees;
+const getEmployeesById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // peticion asincrona
+        const id = parseInt(req.params.id); // parametro url
+        const response = yield db_1.pool.query('SELECT * FROM asesores WHERE id_asesor = $1', [id]);
+        return res.json(response.rows);
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+});
+exports.getEmployeesById = getEmployeesById;
+const createEmployee = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // peticion asincrona
+        const { nombre, apellido1, apellido2, direccion, telefono, email, nivel } = req.body;
+        const response = yield db_1.pool.query('INSERT INTO asesores (nombre, apellido1, apellido2,	direccion , telefono , email , nivel) VALUES ($1, $2, $3, $4, %5, $6, $7)', [nombre, apellido1, apellido2, direccion, telefono, email, nivel]);
+        res.json({
+            message: 'Employee Created',
+            body: {
+                user: { nombre, apellido1, apellido2, direccion, telefono, email, nivel }
+            }
+        });
+        return res.json(response.rows);
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+});
+exports.createEmployee = createEmployee;
+const updateEmployee = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // peticion asincrona
+        const id = parseInt(req.params.id);
+        const { nombre, apellido1, apellido2, direccion, telefono, email, nivel } = req.body;
+        const response = yield db_1.pool.query('UPDATE asesores SET name = $1, apellido1 = $2, apellido2 = $3, direccion = $4, telefono = $5, email = $6, nivel = $7, WHERE id = $8', [nombre, apellido1, apellido2, direccion, telefono, email, nivel, id]);
+        res.json('Employee Updated');
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+});
+exports.updateEmployee = updateEmployee;
+const deleteEmployee = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // peticion asincrona
+        const id = parseInt(req.params.id);
+        yield db_1.pool.query('DELETE FROM asesores where id_asesor = $1', [id]);
+        res.json(`Employee ${id} deleted`);
+    }
+    catch (e) {
+        console.log(e);
+        return res.status(500).json('Internal Server error');
+    }
+});
+exports.deleteEmployee = deleteEmployee;
--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,8 +1,12 @@
 import {Router} from 'express';
 const router = Router();
 
-import { getEmployees } from '../controllers/indexController';
+import { getEmployees, getEmployeesById, createEmployee ,updateEmployee, deleteEmployee} from '../controllers/indexController';
 
-router.get('/getEmployees', getEmployees);
+router.get('/api/employees', getEmployees);
+router.get('/api/employees/getby/:id', getEmployeesById);
+router.post('/api/employees/post', createEmployee);
+router.put('/api/employees/put', updateEmployee)
+router.delete('/api/employees/delby/:id', deleteEmployee);
 
 export default router;
